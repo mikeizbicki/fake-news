eb07a0b4a8da0d90835b001355b17e2423a9cff7
Mike Izbicki <mike@izbicki.me>
2019-10-28 14:54:17 -0700

    first commit

---
+++ CreateXLSX.py
@@ -0,0 +1,320 @@
+import xlsxwriter
+
+# command line args
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--max_doc_size',type=int,default=999999999)
+parser.add_argument('--max_docs',type=int,default=999999999)
+parser.add_argument('--claim',type=str,required=True)
+parser.add_argument('--seed',type=int,default=0)
+parser.add_argument('--labelers',type=str,nargs='+',required=True)
+args = parser.parse_args()
+
+# define column types
+claim_attrs=[]
+claim_attrs.append({
+    'title':'DocType',
+    'validator':{
+        'validate' : 'list',
+        'source' : ['url','tweet']
+        },
+    'width':8,
+    'default':'',
+    'format':{'locked':1},
+    })
+claim_attrs.append({
+    'title':'DocID',
+    'validator':None,
+    'width':8,
+    'default':'',
+    'format':{'locked':1},
+    })
+claim_attrs.append({
+    'title':'Category',
+    'validator':{
+        'validate' : 'list',
+        'source' : ['title','subtitle','body'],
+        },
+    'width':8,
+    'default':'',
+    'format':{'locked':1},
+
+    })
+claim_attrs.append({
+    'title':'Sentence',
+    'validator': None,
+    'width':60,
+    'default':'',
+    'format':{'locked':0},
+
+    })
+#claim_attrs.append({
+    #'title':'Type',
+    #'validator':{
+        #'validate' : 'list',
+        #'source' : [
+            #'1. personal experience',
+            #'2. quantity in the past or present',
+            #'3. correlation or causation',
+            #'4. current laws or rules of operation',
+            #'5. prediction',
+            #'6. other type of claim',
+            #'7. not a claim',
+            #],
+        #},
+    #'width':20,
+    #'default':'',
+    #'format':{'locked':0},
+    #})
+for title in ['Events','Regulations','Quantity','Prediction','Personal','Normative','Other','No Claim']:
+    claim_attrs.append({
+        'title':title,
+        'validator':{
+            'validate' : 'list',
+            'source' : [
+                '',
+                'X',
+                ],
+            },
+        'width':10,
+        'default':'',
+        'special':True,
+        'format':{'locked':0, 'left':1, 'right':1, 'border_color':'#dddddd' },
+        })
+claim_attrs.append({
+    'title':'Citation',
+    'validator':{
+        'validate' : 'list',
+        'source' : [
+            '1. 3rd person source (named)',
+            '2. 3rd person source (anonymous)',
+            '3. 1st person source',
+            '4. news source',
+            '5. other source',
+            '6. no citation',
+            ],
+        },
+    'width':20,
+    'default':'6. no citation',
+    'format':{'locked':0},
+    })
+claim_attrs.append({
+    'title':'Claim Stance',
+    'validator':{
+        'validate' : 'list',
+        'source' : [
+            '1. support',
+            '2. refute',
+            '3. discuss',
+            '4. unrelated'
+            ],
+        },
+    'width':20,
+    'default':'4. unrelated',
+    'format':{'locked':0},
+    })
+claim_attrs.append({
+    'title':'Bias',
+    'validator':{
+        'validate' : 'list',
+        'source' : [
+            '1. in favor of north korea',
+            '2. neutral',
+            '3. against north korea',
+            ],
+        },
+    'width':20,
+    'default':'2. neutral',
+    'format':{'locked':0},
+    })
+#claim_attrs.append({
+    #'title':'Comments',
+    ##'validator':{
+        ##'validate' : 'list',
+        ##'source' : [
+            ##'1. no',
+            ##'2. yes',
+            ##],
+        ##},
+    ##'default':'1. no',
+    #'validator':None,
+    #'default':'',
+    #'width':20,
+    #'format':{'locked':0},
+    #})
+#claim_attrs.append({
+    #'title':'Doc Stance',
+    #'validator':{
+        #'validate' : 'list',
+        #'source' : [
+            #'1. support',
+            #'2. refute',
+            #'3. discuss',
+            #'4. unrelated'
+            #],
+        #},
+    #'width':20,
+    #'default':'4. unrelated',
+    #'format':{'locked':0},
+    #})
+#claim_attrs.append({
+    #'title':'Important',
+    #'validator':{
+        #'validate' : 'list',
+        #'source' : ['high','low'],
+        #},
+    #'width':20,
+    #'default':'low'
+    #})
+
+class MySheet:
+    def __init__(self,filename,columns,title):
+        self.workbook = xlsxwriter.Workbook(filename)
+        self.columns=columns
+        self.doc_row_start=0
+        self.worksheet = self.workbook.add_worksheet()
+        self.current_row=2
+        self.current_doc=0
+
+        center = self.workbook.add_format({'align':'center'})
+        center_bold = self.workbook.add_format({'align':'center','bold': True})
+
+        self.worksheet.merge_range(0,0,0,len(self.columns),title,center_bold)
+        for i in range(len(self.columns)):
+            self.worksheet.set_column(i,i,self.columns[i]['width'])
+            if 'special' not in columns[i]:
+                self.worksheet.merge_range(1,i,2,i,columns[i]['title'],center)
+            else:
+                self.worksheet.write(2,i,self.columns[i]['title'],center)
+        self.worksheet.merge_range(1,4,1,11,'Claim Type',center)
+        self.worksheet.freeze_panes(3,0)
+        self.worksheet.protect()
+
+    def close(self):
+        self.workbook.close()
+
+    def add_validation_rows(self):
+        max_options=20 #FIXME
+        self.current_row+=10
+        for opt in range(max_options):
+            self.current_row+=1
+            self.worksheet.write(self.current_row,0,'discard')
+            for i in range(1,len(self.columns)):
+                try:
+                    self.worksheet.write(self.current_row,i,self.columns[i]['validator']['source'][opt])
+                except:
+                    pass
+
+    def get_format(self,col_id,locked_overwrite=False):
+        if self.current_doc%2==0:
+            if (self.doc_row_start-self.current_row)%2==0:
+                color='#ffeeee'
+            else:
+                color='#ffdddd'
+        else:
+            if (self.doc_row_start-self.current_row)%2==0:
+                color='#eeeeff'
+            else:
+                color='#ddddff'
+        format_dict={
+            'text_wrap': 1, 
+            'valign': 'top', 
+            'bg_color': color,
+            }
+        if 'format' in self.columns[col_id].keys():
+            format_dict.update(self.columns[col_id]['format'])
+        if locked_overwrite:
+            #format_dict['format']={'locked':1} #['locked']=1
+            #format_dict['validator']=None
+            format_dict.update({'locked':1})
+        return self.workbook.add_format(format_dict)
+
+    def add_default_row(self):
+        self.current_row+=1
+        self.worksheet.set_row(self.current_row,45)
+        for i in range(len(self.columns)):
+            self.worksheet.write(self.current_row,i,self.columns[i]['default'],self.get_format(i))
+            if self.columns[i]['validator']:
+                self.worksheet.data_validation(self.current_row,i,self.current_row,i,self.columns[i]['validator'])
+
+    def begin_document(self):
+        self.current_doc+=1
+        self.doc_row_start=self.current_row+1
+
+    def merge_document_columns(self,col_name,val):
+        col=None
+        for i in range(len(self.columns)):
+            if self.columns[i]['title']==col_name:
+                col=i
+        self.worksheet.merge_range(self.doc_row_start,col,self.current_row,col,val,self.get_format(col))
+
+    def update_column(self,col_name,val,locked_overwrite=False):
+        col=None
+        for i in range(len(self.columns)):
+            if self.columns[i]['title']==col_name:
+                col=i
+        if len(val)>3*self.columns[col]['width']:
+            height=15*(1+len(val)/self.columns[col]['width'])
+            self.worksheet.set_row(self.current_row,height)
+        self.worksheet.write(self.current_row,col,val,self.get_format(col,locked_overwrite=locked_overwrite))
+
+########################################
+print('downloading URLs')
+import newspaper 
+articles=[]
+with open(args.claim+'.claim') as f:
+    #title=''.join(f.readlines())
+    title=f.readlines()[0].strip()
+    print('title=',title)
+
+with open(args.claim+'.urls') as f:
+    for url in f.readlines()[:args.max_docs]:
+        try:
+            url=url.strip()
+            print('url=',url)
+            article=newspaper.Article(url)
+            article.download()
+            article.parse()
+            articles.append(article)
+        except newspaper.article.ArticleException as e:
+            print('EXCEPTION:',e)
+
+########################################
+print('adding documents to XLSX')
+import random
+def text2sentences(text):
+    import nltk 
+    tokens = nltk.tokenize.sent_tokenize(article.text)
+    sentences=[]
+    for token in tokens:
+        sentences+=list(filter(bool, token.splitlines()))
+    return sentences 
+
+for labeler in args.labelers:
+    filename=args.claim+'.'+labeler+'.xslx'
+    claim_sheet=MySheet(filename,claim_attrs,title)
+    random.Random(args.seed+hash(labeler)).shuffle(articles)
+    for article in articles:
+        #article.nlp()
+        #sentences = nltk.tokenize.sent_tokenize(article.summary)
+        sentences=text2sentences(article.text)
+        claim_sheet.begin_document()
+        claim_sheet.add_default_row()
+        claim_sheet.update_column('Category','title')
+        claim_sheet.update_column('Sentence',article.title)
+        for sentence in sentences[:args.max_doc_size]:
+            claim_sheet.add_default_row()
+            claim_sheet.update_column('Category','body')
+            claim_sheet.update_column('Sentence',sentence)
+        claim_sheet.add_default_row()
+        claim_sheet.update_column('Category','overall')
+        claim_sheet.update_column('Sentence','<<<< rate the article overall >>>>')
+        #claim_sheet.update_column('Type','---- not applicable ----',locked_overwrite=True)
+        #claim_sheet.update_column('Citation','---- not applicable ----',locked_overwrite=True)
+        for title in ['Events','Regulations','Quantity','Prediction','Personal','Normative','Other','No Claim', 'Citation']:
+            claim_sheet.update_column(title,'N/A',locked_overwrite=True)
+        claim_sheet.merge_document_columns('DocID',article.url)
+        claim_sheet.merge_document_columns('DocType','url')
+        #claim_sheet.merge_document_columns('Doc Stance','')
+    claim_sheet.add_validation_rows()
+    claim_sheet.close()
---
+++ README.md
@@ -0,0 +1,237 @@
+# Instructions for completing the fact checking work
+
+Each sentence of an article needs to be classified in 4 separate ways.
+It is important to remember to:
+
+1. complete each classification for each sentence, and
+
+2. to evaluate each sentence by itself and not in a global context.
+
+There is one important exception to rule #2 above:
+If a sentence contains pronounces such as "they" or "it", you should use the context of other sentences to resolve these pronounces to their correct referent when classifying.
+
+## Type
+
+Each claim can be classified into 8 possible types,
+and each claim can possibly have more than 1 type.
+
+**Events:**
+Any claim about a factual event that happened in the past or is happening in the present (but not a prediction about the future).
+The important characteristic of these events is that they can in principle be verified by an external source.
+
+Positive examples:
+
+1. In 1987, North Korean secret agents bombed a South Korean airplane.
+
+1. The United States promised to build two nuclear reactors in North Korea in exchange for a halt of the North's weapons development program.
+
+1. The United States and North Korea are still technically in a state of war.
+
+1. It is not known if Kim Jong Un eats hamburgers.
+
+1. The summit between Kim Jong Un and Trump was cancelled.
+
+**Regulations:**
+Any claim involving either national or international laws/procedures/treaties/agreements/procedures/contract/other formal agreement.
+
+Positive examples:
+
+1. The UN has passed severe sanctions against North Korea.
+
+1. Trump instituted a "travel ban" that makes it illegal for Americans to visit North Korea.
+
+1. North Korea did not violate the 1994 Agreed Framework.
+
+Negative examples:
+
+1. Distrusting North Korean negotiators is a good rule of thumb.
+
+1. Asian cultures bow as a sign of respect. _(Bowing is a custom, but it is not required by law.)_
+
+**Quantity:**
+Any claim involving a quantity or amount, whether the quantity is specified in absolute or relative terms.
+
+Positive examples:
+
+1. The CIA estimates that North Korea has at least 20 nuclear weapons.
+
+1. The Yongbyon nuclear reactor can generate up to 10 kilograms of plutonium per year.
+
+1. North Korea has more nuclear weapons than Israel.
+
+1. There are no nuclear weapons in South Korea.
+
+1. The number of nuclear weapons in North Korea is not known.
+
+Negative examples:
+
+1. In the year 2020, North Korea and the United States will have signed a peace treaty.
+
+**Prediction:**
+Any claim that makes a prediction about the future, or considers counterfactual or conditional statements.
+
+Positive examples:
+
+1. Experts believe that North Korea will never give up its nuclear weapons in negotiations.
+
+1. If the United States invades North Korea, then the North Koreans will launch nuclear weapons against South Korea.
+
+1. The United States would have won the Korean War if China had not intervened.
+
+1. By the year 2020, North Korea will have 100 nuclear weapons.
+
+**Personal:** 
+Any claim that is about a subjective personal experience, 
+and therefore cannot in principle be verified by any external source.
+(Verifying the claim would require direct access to a person's inner thoughts and feelings.)
+
+Positive examples:
+
+1. I felt scared when I first learned about nuclear weapons.
+
+1. My friend visited North Korea as a tourist and loved it.  _(This is not a claim from a first person perspective, but is a claim about a shared subjective experience.)_
+
+Negative examples:
+
+1. In December of 1994, I met with Kim Il Sung. _(This is a claim made from a first person perspective, but it is not about a subjective experience and is therefore in principle verifiable with outside information.)_
+
+**Normative:**
+Any claim that makes an evaluative assumption about what would be better/worse, or what someone should/should not do.
+
+Positive examples:
+
+1. The world would be a better place if President Trump invaded North Korea.
+
+1. The United States should spend more effort on negotiating with North Korea.
+
+1. I wish China were less involved in the US-North Korean negotiations.
+
+1. The Chinese/American negotiations over North Korea have worsened relations between the US and South Korea.
+
+1. American military bases in Korea ensure the security of the South Korean people. _(The implication is that these military bases are a good thing.)_
+
+Negative examples:
+
+1. Launching a preemptive strike against North Korean nuclear facilities would prevent North Korea from developing more advanced nuclear weapons but may escalate into a full war. _(This is a neutral statement that describes the pros and cons of an action, but does not indicate that the speaker has a preference about whether the event takes place.)_
+
+**Other:**
+A claim is being made, but it does not fit into any of the other categories.
+
+Positive examples:
+
+1. North Korean missiles can reach the United States. _(This is a claim about the capability of something.)_
+
+1. Academics believe that the military will launch an airstrike without fully considering the consequences of their actions. _(This is a claim about the behaviour of a large group of people.)_
+
+**None:**
+If no claim is being made, then do not check any of the boxes.
+
+## Citations
+
+An important goal of this project is to identify what types of sources articles about North Korea use.
+I have ranked 
+
+**3rd person source (named):**
+
+Positive examples:
+
+1. According to Mike Pomepeo, "tensions between North Korea and the United States are at an all time high."
+
+1. The US Secretary of State reported that they are investigating recent military maneuvers by North Koreans.
+
+1. Washington Post journalist Carolyn Hax writes that the US is preparing for a military intervention.
+
+**3rd person source (anonymous):**
+
+Positive examples:
+
+1. An unnamed government official states that Trump and Kim had a good meeting.
+
+1. North Korean defectors report that the country is near collapse.
+
+1. Experts believe that North Korea will never give up its nuclear weapons in negotiations.
+
+1. China wants to be involved in negotiations between the US and North Korea, according to a source close to Xi Jinping.
+
+**1st person source:**
+
+Positive examples:
+
+1. On my trip to North Korea, I observed that their economy is doing well.
+
+1. In my discussions with North Korean diplomats, I have learned that they are very meticulous workers.  _(This is not referring to a particular discussion with a North Korean diplomat, but all discussions broadly.)_
+
+Negative examples:
+
+1. In my discussions with an anonymous North Korean diplomat, I learned that North Korea hopes to make a deal with the United States.  _(This should be a 3rd person anonymous source because it is referring to a particular person and event)_
+
+**News source:**
+
+Positive examples:
+
+1. Bloomberg News reports that North Korea has executed a spy.
+
+1. North Korean state media claim President Trump is evil.
+
+Negative examples:
+
+1. Washington Post journalist Carolyn Hax writes that the US is preparing for a military intervention.  _(This does reference a news source, but it also references a person by name, and that takes priority.)_
+
+**Other source:**
+Any reference that does not fall into one of the above categories.
+For example, references to organizations.
+
+Positive examples:
+
+1. According to the CIA, North Korea tortures and executes Christians.
+
+Negative examples:
+
+1. Christian groups claim that Christians are persecuted in North Korea.  _(This is not a citation because it does not reference specific Christian groups.  This should fall into the no citation category.)_
+
+**No citation:**
+
+Any time a claim was made that does not fit into one of the other categories.
+
+## Claim Stance
+
+At the top of the excel file is a reference claim that all of the articles are somehow related to.
+Each sentence of the article should be compared to the reference frame to see if it supports the claim, refutes the claim, discusses the claim, or does not discuss the claim.  
+
+A statement "supports"/"refutes" the reference claim if it provides evidence that "many people" would consider to make the claim stronger/weaker, and does not need to directly address the claim.
+For example, if the reference claim is: "The United States should attack North Korea" 
+then the statement "airstrikes at key nuclear facilities would halt the North Korean weapons development program" would support the reference claim; 
+and the statement "a war on the Korean peninsula would result in millions of civilian casualties" would refute the claim.
+You specifically should not consider whether the argument is good or bad, only whether an argument could be made.
+
+Any statement that discusses the reference claim but does not clearly provide evidence supporting or refuting the claim should be labeled discuss.
+In particular, a claim should be labeled as discuss if it provides no evidence (e.g. "President Trump has ordered the Secretary of Defense to create plans for an invasion of North Korea") or provides contradictory evidence (e.g. "The CIA has recommended a military strike against North Korea, but the Navy has recommended against military action").
+
+## Bias
+
+A statement is "biased" if tries to provoke an emotional responses in the reader by including non-factual information.
+Facts that provoke an emotional response (or selective reporting of facts) do not count as bias.
+
+**pro-North Korea:**
+
+Positive examples:
+
+1. North Korea is a socialist paradise.
+
+Negative examples:
+
+1. There is no homelessness in North Korea.
+
+**anti-North Korea:**
+
+Positive examples:
+
+1. Kim Jong Un is a dictator.
+
+1. North Korea is a terrorist country.
+
+Negative examples:
+
+1. All media in North Korea is controlled by the state.
+
+1. When North Korean secret agents destroyed a South Korean airplane in 1987, the event was widely condemned as a terrorist incident.
